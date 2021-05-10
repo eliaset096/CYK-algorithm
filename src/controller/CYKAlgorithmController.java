298125093fff1f2e7c8e313ab7023d5b55f8e515
eliaset096 <eliaset096@gmail.com>
2021-05-09 22:57:24 -0500

    Se progresó en la interfaz de usuario y en el controlador.

--- src/controller/CYKAlgorithmController.java
+++ src/controller/CYKAlgorithmController.java
@@ -1,18 +1,41 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class CYKAlgorithmController implements Initializable {
 
+    // Atributos
+    @FXML private JFXTextField tfStringW;
+    @FXML private VBox matrixCYKVBox;
+    @FXML private Text txPrintIsGenerate;
+    @FXML private VBox vbGrammar;
+
+    // Inicializador
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
 
 
+    // Métodos
+    public void addStringWHandler(ActionEvent actionEvent) {
+    }
 
+    public void cleanStringWHandler(ActionEvent actionEvent) {
+    }
 
+    public void executeCYK(ActionEvent actionEvent) {
+    }
 
+    public void addProductionHandler(ActionEvent actionEvent) {
+    }
 
+    public void cleanGICHandler(ActionEvent actionEvent) {
+    }
 }
